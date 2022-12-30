876515dcd37c1adc31f2e8eae92ed8eb7df5f2d7
kingebere <gudoski30@gmail.com>
2022-12-30 21:51:55 +0100

    fix:remove brandlogo

--- components/uiElements/index.js
+++ components/uiElements/index.js
@@ -2,7 +2,6 @@ import Button from "./button";
 import Pill from "./pills";
 import Screenshot from "./screenshot";
 import BrandLogo from "./brandLogo"
-// import BrandLogobig from "./brandLogobig"
 
 import Hamburger from "./hamburger";
 import BrandDescription from "./brandDescription";
