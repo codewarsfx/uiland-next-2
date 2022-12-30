d4755431bd332101b8886da84274c034d44e798a
kingebere <gudoski30@gmail.com>
2022-12-30 22:02:21 +0100

    feat:add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import { analytics } from '../firebase';
 import { UserContextProvider } from "../context/authContext";
+import { Analytics } from '@vercel/analytics/react';
 function MyApp({ Component, pageProps }) {
 
   // useEffect(() => {
@@ -11,7 +12,10 @@ function MyApp({ Component, pageProps }) {
 
 
   return (
-    <UserContextProvider><Component {...pageProps} /></UserContextProvider>
+    <UserContextProvider>
+      <Component {...pageProps} />
+      <Analytics />
+      </UserContextProvider>
     
   ) 
 }
