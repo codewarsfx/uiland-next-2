f748ecd04bdfbe721163717995ec57127d52dce5
kingebere <gudoski30@gmail.com>
2023-01-06 12:32:49 +0100

    fix:fix google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,59 +1,53 @@
-import { useEffect } from 'react'
-import '../styles/globals.css'
-import Script from 'next/script'
-import { useRouter } from 'next/router'
-import * as gtag from '../lib/gtag'
+import { useEffect } from "react";
+import "../styles/globals.css";
+import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 import { UserContextProvider } from "../context/authContext";
 import { ScreensContextProvider } from "../context/screensContex";
-import { Analytics } from '@vercel/analytics/react';
-import Head from 'next/head'
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
-
- 
-  const router = useRouter()
-useEffect(() => {
+  const router = useRouter();
+  useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    router.events.on('hashChangeComplete', handleRouteChange)
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-      router.events.off('hashChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <UserContextProvider>
       <ScreensContextProvider>
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="lazyOnload"
-        dangerouslySetInnerHTML={{
-          __html: `
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+
+        <Script id="google-analytics" strategy="lazyOnload">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', '${gtag.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
-          `,
-        }}
-      />
-  <Head>
-                <title>Welcome to Uiland</title>
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
-            </Head>
-      <Component {...pageProps} />
-      <Analytics />
+                `}
+        </Script>
+        <Head>
+          <title>Welcome to Uiland</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <Component {...pageProps} />
+        <Analytics />
       </ScreensContextProvider>
-      </UserContextProvider>
-    
-  ) 
+    </UserContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
