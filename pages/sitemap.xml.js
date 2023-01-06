995b71829bcd18418852c0b3d18ae4648781983e
kingebere <gudoski30@gmail.com>
2023-01-06 11:04:27 +0100

    feat:add sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,44 @@
+import {getScreensData} from "../firebase"
+
+
+const EXTERNAL_DATA_URL = "https://uiland.design";
+
+function generateSiteMap(screens) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    
+     ${screens
+       .map(({ id }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/screens/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const screens = await getScreensData()
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
+
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
