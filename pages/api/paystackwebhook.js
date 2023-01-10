9b06fd511f7c393d805466f3b308c5a5e5786f1f
kingebere <gudoski30@gmail.com>
2023-01-10 16:16:00 +0100

    fix:env issue

--- pages/api/paystackwebhook.js
+++ pages/api/paystackwebhook.js
@@ -6,6 +6,7 @@ export default function handler(req, res) {
     const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)).digest('hex');
     console.log(req.headers)
     console.log(hash)
+    console.log(req.headers['x-paystack-signature'])
     if (hash == req.headers['x-paystack-signature']) {
     // Retrieve the request's body
     const event = req.body;
