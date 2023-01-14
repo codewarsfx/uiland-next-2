88689b5e6edac8695c416e522c87cd8ff5c36ec3
kingebere <gudoski30@gmail.com>
2023-01-14 14:29:20 +0100

    feat:add hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -45,6 +45,21 @@ function MyApp({ Component, pageProps }) {
   analytics.page();
   }}();`}
         </Script>
+        <Script id="hotjar" strategy="lazyOnload">
+          {`
+          <!-- Hotjar Tracking Code for Uiland -->
+          <script>
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:3322744,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          </script>
+          `}
+        </Script>
         <Head>
           <meta charSet="utf-8" />
           <title>Welcome to Uiland</title>
