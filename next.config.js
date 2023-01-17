6e0cfb92f2e577d37091d4ba287488a20c633b32
kingebere <gudoski30@gmail.com>
2023-01-17 02:01:41 +0100

    refactor:testss

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['firebasestorage.googleapis.com','lh3.googleusercontent.com','epcjufipobybxdmcqjgb.supabase.co'],
-   
-  },
-  
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		domains: [
+			'firebasestorage.googleapis.com',
+			'lh3.googleusercontent.com',
+			'epcjufipobybxdmcqjgb.supabase.co',
+		],
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
