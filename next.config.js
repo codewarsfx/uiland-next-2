e1feb69684cf296045310b438e4c5aa1db19f67b
kingebere <gudoski30@gmail.com>
2023-01-17 01:55:40 +0100

    refactor:testss

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	images: {
-		domains: [
-			'firebasestorage.googleapis.com',
-			'lh3.googleusercontent.com',
-			'epcjufipobybxdmcqjgb.supabase.co',
-		],
-	},
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
-	},
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['firebasestorage.googleapis.com','lh3.googleusercontent.com','epcjufipobybxdmcqjgb.supabase.co'],
+   
+  },
+  
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
