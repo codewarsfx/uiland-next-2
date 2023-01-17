b45c7dd54bb3f47e8134db8a4e14d4ada80da5e8
codewarsfx <chiderainnocent001@gmail.com>
2023-01-17 11:45:20 +0100

    abstracted functionalities into the useScreenshot hook

---
+++ components/ScreenShotActions/index.jsx
@@ -0,0 +1,9 @@
+const Actions = () => {
+    
+
+
+    return (
+
+
+    )
+}
--- hooks/useScreenshot.js
+++ hooks/useScreenshot.js
@@ -1,15 +1,34 @@
 import React, { useContext, useEffect, useState } from "react";
-import styled from "styled-components";
-import Link from "next/link";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import { UserContext } from "../../../context/authContext";
-import { DeleteSingleScreens, viewSingleBookmark } from "../../../supabase";
-import EmptyState from "../../../components/EmptyState";
-import { ScreenContext } from "../../../context/screenContext";
+//Next library
+import Router, { useRouter } from "next/router";
 
-export default function IndividualCollections() {
+//All Supabase endpoints
+import {
+	addSingleScreens,
+	DeleteSingleScreens,
+	getScreensProperties,
+	getAllSingleBookmarkNames,
+	DeleteScreens,
+	getAlbumBookmarkId,
+	addBookmark,
+	getProfileByEvent,
+} from "../supabase.js";
 
+//Hooks
+import useModal from "../hooks/useModal";
+
+//Context
+import { UserContext } from "../context/authContext";
+import { ScreenContext } from "../context/screenContext";
+
+// Components
+import { buttonTypes } from "../components/uiElements/button";
+import { mobileCheck } from "../utils/isMobile";
+
+const useScreenshot = (screens) => {
+	const { getId, setGetId } = useContext(ScreenContext);
+	const user = useContext(UserContext);
+	const router = useRouter();
 	const {
 		modalSaveImage,
 		isModalopen,
@@ -19,12 +38,7 @@ export default function IndividualCollections() {
 		isModalLogin,
 		toggleBottomSheet,
 		modalSheet,
-  } = useModal();
-  
-
-
-
-
+	} = useModal();
 
 	// state for the bottomsheet
 	const [openBottomSheet, setOpenBottomSheet] = useState(false);
@@ -32,9 +46,13 @@ export default function IndividualCollections() {
 	//state for the image url
 	const [imageUrl, setImageUrl] = useState("");
 
+	const [displayBasic, setDisplayBasic] = useState(false);
+	const [imageContent, setImageContent] = useState({});
+
 	//state for the filter by category
 	const [inputFilter, setInputFilter] = useState("");
 
+	const [Progress, setProgress] = useState(1);
 	const [headerInfo, setHeaderInfo] = useState({});
 	const [payingUser, setPayingUser] = useState("");
 
@@ -42,6 +60,7 @@ export default function IndividualCollections() {
 	const [getAlbumId, setGetAlbumId] = useState([]);
 
 	// toast state
+	const [toastPendingText, setToastPendingText] = useState("Saving");
 	const [toastSuccessText, setToastSuccessText] = useState("Saved 🎉");
 
 	const [bookmarkk, setBookmarkk] = useState("");
@@ -61,11 +80,7 @@ export default function IndividualCollections() {
 	const [limitedscreens, setLimitedScreens] = useState([]);
 
 	//state to display the  paying banner
-  const [payingbanner, setPayingBanner] = useState("");
-  
-
-
-
+	const [payingbanner, setPayingBanner] = useState("");
 
 	useEffect(() => {
 		async function getPayingUser() {
@@ -162,13 +177,6 @@ export default function IndividualCollections() {
 			setMobile(mobile);
 		};
 	}, []);
-
-
-
-
-
-
-
 
 	const handleClickSubscribeButton = () => {
 		console.log("handleClickSubscribeButton");
@@ -306,7 +314,12 @@ export default function IndividualCollections() {
 		setProgress(3);
 		toastNotification(1);
 	}
-	
+	//util for toast notification
+	const toastNotification = (state) => {
+		setTimeout(() => {
+			setProgress(state);
+		}, 3000);
+	};
 	//adds image album to bookmark
 	async function handleAddToBookMark() {
 		if (user) {
@@ -358,11 +371,6 @@ export default function IndividualCollections() {
 		setOpenBottomSheet(false);
 	}
 
-
-
-
-
-
 	const filtered = searchFilter(limitedscreens, inputFilter);
 	//the list of properties to filter by
 	const elementsCategoryData = [
@@ -381,237 +389,86 @@ export default function IndividualCollections() {
 		"radio button",
 		"dropdown",
 		"copy icon",
-  ];
+	];
 	const dialogFuncMap = {
 		displayBasic: setDisplayBasic,
 	};
 
+	// useEffect(() => {
+	//   const getHeaderInfo = async () => {
+	//     const data = await getProfileByEvent();
+	//     console.log(data);
+	//   };
+	//   getHeaderInfo();
+	// }, []);
 
+	// useEffect(()=>{
+	//   const uniqueResult=([...new Set(selectBookmark)])
+	//   setSelectBookmark(uniqueResult)
+	//     },[selectBookmark])
 
-	const {getId,setGetId} = useContext(ScreenContext)
+	//copies the url
 
-  const router = useRouter();
-  const user = useContext(UserContext);
-  const [screens, setScreens] = useState([]);
-  const [displayBasic, setDisplayBasic] = useState(false);
-  const [imageContent, setImageContent] = useState({});
+	//finds the ids of individual screens that have been bookmarked and stores in an array
+	//I used it to indicate on the frontend what image have been saved
+	// useEffect(() => {
+	//   async function getIndividualScreens() {
+	//     if (user) {
+	//       const data = await getAllSingleBookmarkId(user);
+	//       console.log(data);
+	//       data.forEach((item) => {
+	//         setGetId((prev) => {
+	//           return [...prev, item.screen_id];
+	//         });
+	//       });
+	//     }
+	//   }
+	//   getIndividualScreens();
+	// }, [user]);
 
-  const [Progress, setProgress] = useState(1);
-  const [toastPendingText, setToastPendingText] = useState("Saving");
+	//finds the ids of album screens that have been bookmarked and stores in an array
+	//I used it to indicate on the frontend what image have been saved
 
-
-  useEffect(() => {
-    async function getAlbums() {
-      if (user) {
-      
-        const data = await viewSingleBookmark(router.query.name);
-        console.log(data);
-        setScreens(data);
-      }
-    }
-    getAlbums();
-  }, [router.query.name, user]);
-
-  //copy images
-  async function copyImage(e) {
-    const response = await fetch(
-      e.target.parentElement.children[3].children[1].currentSrc
-    );
-    const blob = await response.blob();
-    navigator.clipboard.write([
-      new window.ClipboardItem({
-        [blob.type]: blob,
-      }),
-    ]);
-    console.log("Image copied.");
-  }
-
-	async function deleteIndividualBookmark(data) {
-		setProgress(2);
-		setToastPendingText("Deleting");
-		const deletedItem = await DeleteSingleScreens(data);
-		console.log(deletedItem);
-		if (deletedItem === null) {
-			console.log(data.id);
-			const filteredResult = getId.filter((result) => result !== data.id);
-			console.log(filteredResult);
-			setGetId(filteredResult);
-			setToastSuccessText("Deleted :(");
-			setProgress(3);
-			toastNotification(1);
-		}
-	}
-
-  const toastNotification = (state) => {
-		setTimeout(() => {
-			setProgress(state);
-		}, 3000);
+	return {
+		headerInfo,
+		toggleBottomSheet,
+		downloadImage,
+		copyImage,
+		modalSheet,
+		modalSaveImage,
+		newtoggleModal,
+		submit,
+		handleChange,
+		selectBookmark,
+		input,
+		disabled,
+		toggleModal,
+		isModalopen,
+		copy,
+		isModalLogin,
+		loginToggleModal,
+		openBottomSheet,
+		closeBottomSheetModal,
+		downloadImage,
+		copyImage,
+		getAlbumId,
+		handleAddToBookMark,
+		handleDeleteFromBookMark,
+		elementsCategoryData,
+		inputFilter,
+		handleInputFilter,
+		filtered,
+		getId,
+		deleteIndividualBookmark,
+		bookmark,
+		openBottomSheetModal,
+		payingbanner,
+		handleClickSubscribeButton,
+		buttonTypes,
+		Progress,
+		toastPendingText,
+		toastSuccessText,
 	};
+};
 
-
-
-
-
-  return (
-    <>
-      <SingleHeader>
-        <>
-          <Title>{router.query.name}</Title>
-
-          <div></div>
-        </>
-      </SingleHeader>
-      <ElementsInCategoryContainer>
-        {screens ? (
-          screens?.map((data) => (
-            <>
-            <ScreenshotContainer key={data.url}>
-              {/* add the name to alt tag */}
-              <Image
-                src={data.screen_id.url}
-                alt={`Screenshots of  App`}
-                width={1080}
-                height={2240}
-              />
-            </ScreenshotContainer>
-            <SecondRow>
-            {getId.includes(data.id) ? (
-              <DeleteIcon
-                deleteIndividualBookmark={deleteIndividualBookmark}
-                data={data}
-              />
-            ) : (
-              <SaveIcon bookmark={bookmark} data={data} />
-            )}
-            <ThreeDots openBottomSheet={openBottomSheetModal} />
-              </SecondRow>
-              </>
-          ))
-        ) : (
-          <EmptyState/>
-        )}
-      </ElementsInCategoryContainer>
-    </>
-  );
-}
-const ModalBox = styled.div`
-  background-color: #fff;
-  max-width: 37.5rem;
-  padding: 1.6rem;
-  border-radius: 0.5rem;
-`;
-
-const DownloadWrapper = styled.div`
-  position: absolute;
-  block: "";
-  z-index: 99;
-  display: flex;
-  flex-direction: column;
-  padding: 7px 16px;
-  align-items: flex-start;
-  top: 10px;
-  right: 94px;
-  border-radius: 2em;
-  visibility: hidden;
-`;
-const CopyWrapper = styled.div`
-  position: absolute;
-  block: "";
-  z-index: 99;
-  display: flex;
-  flex-direction: column;
-  padding: 7px 16px;
-  align-items: flex-start;
-  top: 10px;
-  right: 18px;
-  border-radius: 2em;
-  visibility: hidden;
-`;
-
-const AbsoluteBox = styled.div`
-  position: absolute;
-  block: "";
-  z-index: 99;
-  display: flex;
-  flex-direction: column;
-  padding: 7px 16px;
-  align-items: flex-start;
-  top: 10px;
-  right: 54px;
-  border-radius: 2em;
-  visibility: hidden;
-`;
-const ScreenshotContainer = styled.div`
-  border-radius: 0.8em;
-  background: linear-gradient(to bottom, white 99%, black 1%);
-  overflow: auto;
-  border: 1px solid #dce0f1;
-  border-radius: 20px;
-  position: relative;
-  cursor: pointer;
-  user-select: none;
-  &:hover .target {
-    visibility: visible;
-  }
-  img {
-    pointer-events: none;
-  }
-`;
-
-const Title = styled.h1`
-  z-index: 99;
-  font-size: 12px;
-  font-weight: 300;
-  margin: 0;
-  padding: 5px;
-  position: absolute;
-  block: "";
-  border-radius: 5px;
-  background: rgba(0, 0, 0, 0.17);
-  z-index: 99;
-  top: 0;
-  right: 0;
-  visibility: hidden;
-  svg {
-    width: 23px;
-    height: 23px;
-    vertical-align: middle;
-  }
-  img {
-    height: 20px;
-    width: 20px;
-    transition: all 0.5s ease-out;
-  }
-`;
-const SingleHeader = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 15px;
-  gap: 8px;
-`;
-
-const WebLink = styled.a`
-  font-weight: 200;
-  font-size: 1.3rem;
-  text-decoration: none;
-  color: var(--primary-color);
-`;
-const ElementsInCategoryContainer = styled.div`
-  position: relative;
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  margin: 1.5em auto;
-  gap: 20px;
-  width: 90%;
-  @media (min-width: 540px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (min-width: 768px) {
-    width: 95%;
-    margin: 3em auto;
-    gap: 20px;
-    grid-template-columns: repeat(4, 1fr);
-  }
-`;
+export default useScreenshot
