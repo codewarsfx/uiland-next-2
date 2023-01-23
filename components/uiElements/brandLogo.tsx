49d332801853605f4e81ed2c97f31a7200f591fa
kingebere <gudoski30@gmail.com>
2023-01-23 01:03:46 +0100

    fix:adjust header-homepage

--- components/uiElements/brandLogo.tsx
+++ components/uiElements/brandLogo.tsx
@@ -8,17 +8,19 @@ const BrandLogo = ({ imageUrl }) => {
 };
 
 const BrandLogoCotainer = styled.div`
-	width: 30px;
-	height: 30px;
-	border-radius: 1em;
+	width: 40px;
+	height: 40px;
+	border-radius: 12px;
+	background: white;
+	padding: 9px;
+	border: 1px solid #dddddd;
 	img {
 		height: 100%;
 		width: 100%;
-		border-radius: 1em;
 	}
 	@media (min-width: 768px) {
-		width: 45px;
-		height: 45px;
+		width: 50px;
+		height: 50px;
 	}
 `;
 
