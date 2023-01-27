efe084b61a96c115d06b7462abba0c38cc165d89
kingebere <gudoski30@gmail.com>
2023-01-27 22:55:42 +0100

    fix:add new filters

--- components/uiElements/screenshot.tsx
+++ components/uiElements/screenshot.tsx
@@ -14,9 +14,9 @@ const Screenshot = ({ imgLink, Name }) => {
 
 const ScreenshotContainer = styled(motion.div)`
 	border-radius: 0.8em;
-	background-color: var(--light-grey-color);
 	cursor: pointer;
 	border: 1px solid #dddddd;
+	
 	max-height: 634px;
 	img {
 		height: 100%;
