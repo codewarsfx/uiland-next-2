850b3b3835a8f84d1e2a89891cb18502128a6453
codewarsfx <chiderainnocent001@gmail.com>
2023-02-03 17:05:50 +0100

    redirect based on process environment

--- supabase.ts
+++ supabase.ts
@@ -5,24 +5,28 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 //supabase signup with Google Oauth
-const getURL = () => {
-	let url =
-		process?.env?.NEXT_PUBLIC_SITE_URL && // Set this to your site URL in production env.
-		process?.env?.NEXT_PUBLIC_SITE_URL && // Automatically set by Vercel.
-		'http://localhost:3000/';
+// const getURL = () => {
+// 	let url =
+// 		process?.env?.NEXT_PUBLIC_SITE_URL && // Set this to your site URL in production env.
+// 		process?.env?.NEXT_PUBLIC_SITE_URL && // Automatically set by Vercel.
+// 		'http://localhost:3000/';
 
-	// Make sure to include `https://` when not localhost.
-	url = url.includes('http') ? url : `https://${url}`;
-	// Make sure to including trailing `/`.
-	url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
-	return url;
-};
+// 	// Make sure to include `https://` when not localhost.
+// 	url = url.includes('http') ? url : `https://${url}`;
+// 	// Make sure to including trailing `/`.
+// 	url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+// 	return url;
+// };
 
 export async function signInWithGoogle() {
+
+	let redirectUrl = process.env.NODE_ENV === 'production' ? 'https://uiland.design' : 'http://localhost:3000/'
+
+	
 	const { data, error } = await supabase.auth.signInWithOAuth({
 		provider: 'google',
 		options: {
-			redirectTo: getURL(),
+			redirectTo: redirectUrl,
 		},
 	});
 	return data;
