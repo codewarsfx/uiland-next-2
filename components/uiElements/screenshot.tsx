4480d49d819c8b2b585f21414be577927fb6a08c
Dike Goodluck <gudoski30@gmail.com>
2023-02-16 18:54:58 +0100

    Revert "type"

--- components/uiElements/screenshot.tsx
+++ components/uiElements/screenshot.tsx
@@ -2,14 +2,7 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 
-import React from 'react';
-
-type screenshotProps = {
-	imgLink: '';
-	Name: '';
-};
-
-const Screenshot: React.FC<screenshotProps> = ({ imgLink, Name }) => {
+const Screenshot = ({ imgLink, Name }) => {
 	return (
 		<>
 			<ScreenshotContainer initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
