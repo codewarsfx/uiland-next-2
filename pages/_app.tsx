7048f9aba04be4adc9fa6db37237efccc92fdce1
kingebere <gudoski30@gmail.com>
2023-02-16 20:53:47 +0100

    feat:track users downloads and copy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -110,8 +110,9 @@ export default function App({
 							content='Discover hundreds of Mobile apps designs for UI &amp; UX research.'
 						/>
 						<meta
-							name='twitter:image:src'
-							content='https://epcjufipobybxdmcqjgb.supabase.co/storage/v1/object/public/uiland-store/uiland-capture2.PNG'
+							name="twitter:image"
+							
+							content="https://epcjufipobybxdmcqjgb.supabase.co/storage/v1/object/public/uiland-store/uiland-capture2.PNG"
 						/>
 					
 						<link
