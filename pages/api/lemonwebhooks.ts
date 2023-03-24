0ccf5eeaa6cbbab02957b8f63123a99bc313b35a
kingebere <gudoski30@gmail.com>
2023-03-24 02:22:26 +0100

    feat:webhooks

--- pages/api/lemonwebhooks.ts
+++ pages/api/lemonwebhooks.ts
@@ -25,36 +25,7 @@ async function handler(req, res) {
 console.log(req.body)
     if (req.body.meta["event_name"] === 'subscription_created'||req.body.meta["event_name"] === 'subscription_updated') {
         // update the profile table when a "subscription.create" is available
-        const insertToProfile = async () => {
-            const { data, error } = await supabase
-                .from('profile')
-                .update({
-                    event: 'subscription.create',
-                    status: 'active',
-                    amount: req.body.data.attributes.total,
-                    currency: req.body.data.attributes.currency,
-                    authorization_code:'',
-                    next_payment_date: req.body.data.attributes.ends_at,
-                    created_date_at:req.body.data.attributes.created_at,
-                    exp_month: req.body.data.attributes.ends_at,
-                    plan_name:req.body.data.attributes.product_name,
-                    plan_interval:'',
-                    signature:'',
-                    bank: req.body.data.attributes.card_brand,
-                    card_type:  req.body.data.attributes.card_brand,
-                    brand: req.body.data.attributes.card_brand,
-                    subscription_code:'',
-                })
-                //This is the bridge between the response from paystack and our database (the email is the same in both)
-                .eq('email', req.body.data.attributes.user_email)
-                .select();
-      
-            return res.status(200).json({
-                status: true,
-                message: 'Order placed successfully!',
-            });
-        };
-        insertToProfile();
+     console.log("yes")
     }  
     }
     }catch (error) {
